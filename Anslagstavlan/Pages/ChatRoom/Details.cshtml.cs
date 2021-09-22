1299fa3dda90ad31082135582fc192e4bc3eab6c
Andreas Lind <Andreaslind31@gmail.com>
2021-09-22 15:51:41 +0200

    renamed a bunch + scaffolded index, details razor pages

---
+++ Anslagstavlan/Pages/ChatRoom/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Anslagstavlan.Domain.Database;
+using Anslagstavlan.Domain.Models;
+
+namespace Anslagstavlan.Pages.ChatRoom
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Anslagstavlan.Domain.Database.AuthDbContext _context;
+
+        public DetailsModel(Anslagstavlan.Domain.Database.AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public ChatRoomModel ChatRoomModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ChatRoomModel = await _context.ChatRoomModels.FirstOrDefaultAsync(m => m.ChatRoomId == id);
+
+            if (ChatRoomModel == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Anslagstavlan/Pages/ChatRoom/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Anslagstavlan.Domain.Database;
+using Anslagstavlan.Domain.Models;
+
+namespace Anslagstavlan.Pages.ChatRoom
+{
+    public class ListRoomModel : PageModel
+    {
+        private readonly Anslagstavlan.Domain.Database.AuthDbContext _context;
+
+        public ListRoomModel(Anslagstavlan.Domain.Database.AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ChatRoomModel> ChatRoomModel { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            ChatRoomModel = await _context.ChatRoomModels.ToListAsync();
+        }
+    }
+}
